6d2f10323332ab32708241bcbb36a59a9a015732
jaeshim <jaeshim@gmarket.com>
2022-08-13 11:14:08 +0900

    initial

---
+++ src/test/java/com/jaeshim/kafka/producer/performance/ProducerPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaeshim.kafka.producer.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
